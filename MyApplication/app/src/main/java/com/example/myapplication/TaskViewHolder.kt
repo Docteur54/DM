20ab3774847ff6e7011e33cb007b691b6e132940
Elio de Gani <38035121+iieBaggins@users.noreply.github.com>
2019-12-26 15:55:36 +0100

    Fixed Task data display
    - TaskViewHolder.kt : bind function implemented

--- MyApplication/app/src/main/java/com/example/myapplication/TaskViewHolder.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/TaskViewHolder.kt
@@ -1,10 +1,30 @@
 package com.example.myapplication
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
 
-    fun bind(task : Task){}
+    private val title: TextView = itemView.findViewById(R.id.task_title)
+    private val description: TextView = itemView.findViewById(R.id.task_description)
+    private val id: TextView = itemView.findViewById(R.id.task_id)
+
+    fun bind(task : Task){
+        if(task == null)
+        {
+            val resources = itemView.resources
+            title.text = resources.getString(R.string.loading_message)
+        }
+        else
+        {
+            title.text = task.title
+            title.visibility = View.VISIBLE
+            description.text = task.description
+            description.visibility = View.VISIBLE
+            id.text = task.id
+            id.visibility = View.VISIBLE
+        }
+    }
 
 }
